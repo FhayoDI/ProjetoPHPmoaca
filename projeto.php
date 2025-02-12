4a193e8ebf8d967df5838c4b09b14873ceafba3c
FhayoDI <fhayodi@gmail.com>
2025-02-12 08:35:56 -0300

    primeira versao do menu de vendas, sem funçoes

---
+++ projeto.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$vendas = 0;
+$user = readline();
+$id =  0 ;
+$menuinicial = 0;
+$escolhaMenu = 0;
+while ($escolhaMenu != 4) {
+    echo "\n============================================================================================";
+    echo "Bem vindo $user, você esta logado com o ID: $id!   Seu número total em vendas é: $vendas\n";
+    echo "Selecione uma das opções a seguir:\n";
+    echo "1 = Vender \n";
+    echo "2 = Cadastrar novo usuário \n";
+    echo "3 = Verificar a Log \n";
+    echo "4 = Deslogar \n" ;
+    echo "\n============================================================================================";
+    $escolhaMenu = readline();
+    if ($escolhaMenu == 1){
+
+    }
+}
+
+
+
+
+
+
