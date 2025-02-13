be905748e9ae9a727a13a91ba2cfa25daf9da41d
FhayoDI <fhayodi@gmail.com>
2025-02-13 09:03:42 -0300

    sistema de logs em desenvolvimento, e uma modificação de emeblezamento no registro

--- projeto.php
+++ projeto.php
@@ -1,39 +1,70 @@
 <?php
 //login
 // Registro
-function registro(){
-    global $DatabaseUsers;
-    global $password;
-    echo "Registro de usuários: \n";
-    $userRegistro = readline("Nome de usuário");
-    $password = readline("Senha");
-    $DatabaseUsers[]=[$userRegistro, $password];
-    $msg = "$userRegistro" . "$password";
-    file_put_contents('usuarios.txt ', $msg);
-}
-//FUNÇÃO VENDER
-function vender() {
-echo "\nVocê está no menu de vendas do sistema";
-do {
-    global $user;
-    global $produto;
-    global $vendas;
-    echo "\nInsira o produto e o preço:\n";
-    $produto = readline("Produto:");
-    $preco = readline("Preço:");
-    $quantia = readline("Quantia");
-    $vendas += $preco;
-    global $databaseProducts;
-    $databaseProducts[]= [$user,$produto, $preco];
-    $msg = "$user vendeu " . $quantia." " . $produto . " por " . $preco . " R$";
-    file_put_contents('historico.txt', $msg, FILE_APPEND);
-    echo "realizar outra venda? \n Digite sim ou não.";
-    echo "\n";
-    $sair = readline();
-    system("clear");
-} while ($sair === "sim");
+function registro()
+{
+    do {
+        global $DatabaseUsers;
+        global $password;
+        echo "Registro de usuários: \n";
+        $userRegistro = readline("Nome de usuário: ");
+        $password = readline("Senha: ");
+        $DatabaseUsers[] = [$userRegistro, $password];
+        $msg = "Usuario: " . "$userRegistro" . "Senha:  " . "$password" . "\n";
+        file_put_contents('usuarios.txt ', $msg, FILE_APPEND);
+        echo "usuario registrado com sucesso, acesso ao sistema liberado!\n";
+        echo "cadastrar outro usuario?\n";
+        $userloop = readline("digite sim ou não\n");
+        if ($userloop === "sim")
+            system("clear");
+    } while ($userloop === "sim");
 }
 
+//FUNÇÃO VENDER
+function vender()
+{
+    echo "\nVocê está no menu de vendas do sistema";
+    do {
+        global $user;
+        global $produto;
+        global $vendas;
+        echo "\nInsira o produto e o preço:\n";
+        $produto = readline("Produto:");
+        $preco = readline("Preço:");
+        $quantia = readline("Quantia");
+        $vendas += $preco;
+        global $databaseProducts;
+        $databaseProducts[] = [$user, $produto, $preco];
+        $msg = "$user vendeu " . $quantia . " " . $produto . " por " . $preco . " R$";
+        file_put_contents('historico.txt', $msg, FILE_APPEND);
+        echo "realizar outra venda? \n Digite sim ou não.";
+        echo "\n";
+        $sair = readline();
+        system("clear");
+    } while ($sair === "sim");
+}
+
+// LOGS
+function logs()
+{
+    do {
+        $arquivo = 0;
+        $loguser = 0;
+        $loghistorico = 0;
+        $choice = readline("Digite user para log de usuarios e hist para o historico de vendas!\n Ou digite sair para voltar ao menu inicial");
+        if ($choice === "user") {
+            function usuarios()
+            {
+
+            }
+        } elseif ($choice === "hist") {
+            function historico()
+            {
+
+            }
+        }
+    } while ($choice != "sair");
+}
 
 // MENU PRINCIPAL
 do {
@@ -45,10 +76,14 @@ do {
     echo "4 = Deslogar \n";
     $escolhaMenu = readline("digite:");
     if ($escolhaMenu == 1) {
-    vender();
+        vender();
     }
-    if ($escolhaMenu == 2);{
+    if ($escolhaMenu == 2)
+    {
         registro();
+    }
+    if ($escolhaMenu == 3) {
+        logs();
     }
 } while ($escolhaMenu != 4);
 
